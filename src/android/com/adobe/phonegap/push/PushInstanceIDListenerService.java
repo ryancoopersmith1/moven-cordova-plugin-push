5617c1647f20a3c6ec88d8c33226963605383a7d
Ryan Coopersmith <ryanc@moven.com>
2019-08-20 15:36:50 -0400

    start working on making API call

--- src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
+++ src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
@@ -27,6 +27,28 @@ public class PushInstanceIDListenerService extends FirebaseInstanceIdService imp
         SharedPreferences prefs = getApplicationContext().getSharedPreferences(COM_ADOBE_PHONEGAP_PUSH,
             Context.MODE_PRIVATE);
         String oldToken = prefs.getString(FCM_TOKEN, null);
-        // API call to new endpoint to find old token and update with new token
+
+        String appHost = getResources()
+            .getIdentifier("app_host", "", null);
+        String url = appHost + "/pay/device/refresh";
+
+        // JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+        //         (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+        //     @Override
+        //     public void onResponse(JSONObject response) {
+        //         textView.setText("Response: " + response.toString());
+        //     }
+        // }, new Response.ErrorListener() {
+
+        //     @Override
+        //     public void onErrorResponse(VolleyError error) {
+        //         // TODO: Handle error
+
+        //     }
+        // });
+
+        // // Access the RequestQueue through your singleton class.
+        // MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 }
