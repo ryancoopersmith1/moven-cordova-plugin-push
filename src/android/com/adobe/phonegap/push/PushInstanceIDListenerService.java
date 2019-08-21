b7b6a460560fb9a6757196517a44ac32c9b36fc4
Ryan Coopersmith <ryanc@moven.com>
2019-08-21 16:00:18 -0400

    lots of progress!!!

--- src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
+++ src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.os.Bundle;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 public class PushInstanceIDListenerService extends FirebaseInstanceIdService implements PushConstants {
     public static final String LOG_TAG = "Push_InsIdService";
@@ -24,31 +28,52 @@ public class PushInstanceIDListenerService extends FirebaseInstanceIdService imp
     }
 
     public void sendRegistrationToServer(String token) {
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences(COM_ADOBE_PHONEGAP_PUSH,
+        Context context = getApplicationContext();
+
+        SharedPreferences prefs = context.getSharedPreferences(COM_ADOBE_PHONEGAP_PUSH,
             Context.MODE_PRIVATE);
         String oldToken = prefs.getString(FCM_TOKEN, null);
 
-        String appHost = getResources()
-            .getIdentifier("app_host", "", null);
-        String url = appHost + "/pay/device/refresh";
+        if (oldToken.isEmpty()) {
+            return;
+        }
 
-        // JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-        //         (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        JSONObject body = new JSONObject();
+        body.put("token", token);
+        body.put("oldToken", oldToken);
 
-        //     @Override
-        //     public void onResponse(JSONObject response) {
-        //         textView.setText("Response: " + response.toString());
-        //     }
-        // }, new Response.ErrorListener() {
+        String appHost = context.getResources()
+            .getString(R.string.app_host);
+        String urlString = appHost.concat("/pay/device/refresh");
 
-        //     @Override
-        //     public void onErrorResponse(VolleyError error) {
-        //         // TODO: Handle error
+        HttpURLConnection conn;
+        URL url = new URL(urlString);
+        conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
 
-        //     }
-        // });
+        conn.setRequestMethod("PUT");
+        conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+        conn.setRequestProperty("Accept", "*/*");
+        conn.setRequestProperty("x-moven-channel", "mobile");
+        conn.setRequestProperty("x-moven-osversion", String.valueOf(android.os.Build.VERSION.SDK_INT));
+        conn.setRequestProperty("x-moven-appos","android");
 
-        // // Access the RequestQueue through your singleton class.
-        // MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        if (body != null) {
+            conn.setRequestProperty("Content-Length", Integer.toString(body.toString().getBytes().length));
+        }
+
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+
+        sendRequest(body, conn);
+    }
+
+    public static void sendRequest(JSONObject body, HttpURLConnection conn) throws IOException {
+        DataOutputStream wr = new DataOutputStream(
+                conn.getOutputStream ());
+        wr.writeBytes(body.toString());
+        wr.flush();
+        wr.close();
     }
 }
