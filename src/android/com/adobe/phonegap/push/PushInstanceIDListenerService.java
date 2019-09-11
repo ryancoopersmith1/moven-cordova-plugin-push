fe6e9a6ad4aaa4f3c9ac4a81f97fc2b2bf8f3711
Ryan Coopersmith <ryanc@moven.com>
2019-09-11 11:40:14 -0400

    add: FCM token refresh support

--- src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
+++ src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
@@ -1,16 +1,58 @@
 package com.adobe.phonegap.push;
 
+import ANDROID_APP_ID.R;
+
 import android.content.Intent;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.os.Bundle;
+import android.os.AsyncTask;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.lang.InterruptedException;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class ForegroundCheckTask extends AsyncTask<Context, Void, Boolean> {
+
+    @Override
+    protected Boolean doInBackground(Context... params) {
+        final Context context = params[0].getApplicationContext();
+        return isAppOnForeground(context);
+    }
+
+    private boolean isAppOnForeground(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
+        if (appProcesses == null) {
+            return false;
+        }
+
+        final String packageName = context.getPackageName();
+        for (RunningAppProcessInfo appProcess : appProcesses) {
+            if (appProcess.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND && appProcess.processName.equals(packageName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
 
 public class PushInstanceIDListenerService extends FirebaseInstanceIdService implements PushConstants {
     public static final String LOG_TAG = "Push_InsIdService";
@@ -20,7 +62,92 @@ public class PushInstanceIDListenerService extends FirebaseInstanceIdService imp
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
-        // TODO: Implement this method to send any registration to your app's servers.
-        //sendRegistrationToServer(refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        if (token == null || token.isEmpty()) {
+            Log.d(LOG_TAG, "Empty registration ID received from FCM");
+            return;
+        }
+
+        Context context = getApplicationContext();
+
+        try {
+            boolean foregroud = new ForegroundCheckTask().execute(context).get();
+            if (foregroud) {
+                Log.d(LOG_TAG, "App is in foreground. Not updating FCM token");
+                return;
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        SharedPreferences prefs = context.getSharedPreferences(PushPlugin.COM_ADOBE_PHONEGAP_PUSH,
+            Context.MODE_PRIVATE);
+        String oldToken = prefs.getString(REGISTRATION_ID, null);
+
+        Log.d(LOG_TAG, "Old FCM token to update: " + oldToken);
+        if (oldToken == null || oldToken.isEmpty()) {
+            return;
+        }
+
+        try {
+            JSONObject body = new JSONObject();
+            body.put("token", token);
+            body.put("oldToken", oldToken);
+
+            String appHost = context.getResources()
+                .getString(R.string.app_host);
+            String urlString = appHost.concat("/pay/device/refresh");
+
+            Log.d(LOG_TAG, "Sending request to " + urlString + " to update FCM token");
+
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            conn.setRequestMethod("PUT");
+            conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            conn.setRequestProperty("Accept", "*/*");
+
+            conn.setRequestProperty("x-moven-channel", "mobile");
+            conn.setRequestProperty("x-moven-osversion", String.valueOf(android.os.Build.VERSION.SDK_INT));
+            conn.setRequestProperty("x-moven-appos","android");
+
+            if (body != null) {
+                conn.setRequestProperty("Content-Length", Integer.toString(body.toString().getBytes().length));
+            }
+
+            sendRequest(body, conn);
+
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode >= 200 && responseCode <= 299) {
+                Log.d(LOG_TAG, "Successfully saved refreshed token in server. Response code: " + responseCode);
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString(REGISTRATION_ID, token);
+                editor.apply();
+            } else {
+                Log.e(LOG_TAG, "Failed to save refreshed token in server. Response code: " + responseCode);
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendRequest(JSONObject body, HttpURLConnection conn) throws IOException {
+        DataOutputStream wr = new DataOutputStream(
+                conn.getOutputStream ());
+        wr.writeBytes(body.toString());
+        wr.flush();
+        wr.close();
     }
 }
